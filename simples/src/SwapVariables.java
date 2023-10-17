7c582713b0e032587c84438e6c7af51af0407787
davidajimati <ajimatidavid@gmail.com>
2023-10-17 08:28:27 +0100

    input average and swap variables in process

---
+++ simples/src/SwapVariables.java
@@ -0,0 +1,10 @@
+public class SwapVariables {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 6;
+        int temp = a;
+        a = b;
+        b =  temp;
+        System.out.println(a + " " + b);
+    }
+}

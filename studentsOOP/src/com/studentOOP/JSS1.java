dfe634187df71919b5a87cb89e27b4030881dc4b
davidajimati <ajimatidavid@gmail.com>
2023-10-19 16:34:59 +0100

    adjusted the private variables to have underscores

--- studentsOOP/src/com/studentOOP/JSS1.java
+++ studentsOOP/src/com/studentOOP/JSS1.java
@@ -3,14 +3,13 @@ package com.studentOOP;
 public class JSS1 {
     static String grade = "JSS1";
     public String studentID;
-    private int age;
+    private int _age;
     String name;
-    int position;
-
+    private int _position;
     public JSS1(int age, String name, int position) {
-        this.age = age;
+        this._age = age;
         this.name = name;
-        this.position = position;
+        this._position = position;
     }
 
     // use setters and getters for age and position
@@ -18,23 +17,23 @@ public class JSS1 {
 
     int getAge(String ID) {
         if (ID == this.studentID)
-            return this.age;
+            return this._age;
         return 0;
     }
 
     void setAge(int value) {
-        this.age = value;
+        this._age = value;
     }
 
     int getPosition(String ID) {
         if (ID == this.studentID)
-                return this.position;
+                return this._position;
         return 0;
     }
 
     void setPosition(String ID, int val) {
         if (ID == this.studentID)
-            this.position = val;
+            this._position = val;
     }
 
     String getStudentID() {
@@ -44,7 +43,7 @@ public class JSS1 {
         String firstChar = String.valueOf(this.name.charAt(0));
         String MidChar = String.valueOf(this.name.charAt(name.length() / 2));
         String lastChar = String.valueOf(this.name.charAt(this.name.length() - 1));
-        int figure = (14567 / this.age);
+        int figure = (14567 / this._age);
         return((String)firstChar+MidChar+lastChar+figure).toUpperCase();
     }
 }

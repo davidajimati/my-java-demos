5371694917ee9cf5fed2b87cb48902f8be1bcd46
davidajimati <ajimatidavid@gmail.com>
2023-10-19 16:01:03 +0100

    added getters and setters for postion and age

--- studentsOOP/src/com/studentOOP/JSS1.java
+++ studentsOOP/src/com/studentOOP/JSS1.java
@@ -3,7 +3,7 @@ package com.studentOOP;
 public class JSS1 {
     static String grade = "JSS1";
     public String studentID;
-    int age;
+    private int age;
     String name;
     int position;
 
@@ -12,10 +12,38 @@ public class JSS1 {
         this.name = name;
         this.position = position;
     }
+
+    // use setters and getters for age and position
+    // verify if the studentID is correct before returning the position
+
+    int getAge(String ID) {
+        if (ID == this.studentID)
+            return this.age;
+        return 0;
+    }
+
+    void setAge(int value) {
+        this.age = value;
+    }
+
+    int getPosition(String ID) {
+        if (ID == this.studentID)
+                return this.position;
+        return 0;
+    }
+
+    void setPosition(String ID, int val) {
+        if (ID == this.studentID)
+            this.position = val;
+    }
+
+    String getStudentID() {
+        return this.studentID;
+    }
     public String IdEngine() {
         String firstChar = String.valueOf(this.name.charAt(0));
-        String MidChar = String.valueOf(name.charAt(name.length() / 2));
-        String lastChar = String.valueOf(name.charAt(4));
+        String MidChar = String.valueOf(this.name.charAt(name.length() / 2));
+        String lastChar = String.valueOf(this.name.charAt(this.name.length() - 1));
         int figure = (14567 / this.age);
         return((String)firstChar+MidChar+lastChar+figure).toUpperCase();
     }
--- studentsOOP/src/com/studentOOP/Jss1Methods.java
+++ studentsOOP/src/com/studentOOP/Jss1Methods.java
@@ -6,10 +6,15 @@ public class Jss1Methods {
         david.studentID = david.IdEngine();
 
         JSS1 samson = new JSS1(34, "Samson", 4);
-        david.studentID = samson.IdEngine();
+        samson.studentID = samson.IdEngine();
+
+        JSS1 tolu = new JSS1(14, "toluwalase", 1);
+        tolu.studentID = tolu.IdEngine();
 
 
         System.out.println("David's ID; " + david.studentID);
         System.out.println("Samson's ID; " + samson.studentID);
+        System.out.println("Tolu's ID; " + tolu.studentID);
+        System.out.println("David's postion: " + david.getPosition(david.studentID));
     }
 }

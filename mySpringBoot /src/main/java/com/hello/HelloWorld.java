33cdf93170877ba9d2c691dec40bb47dd1a6c909
davidajimati <ajimatidavid@gmail.com>
2023-10-23 10:38:04 +0100

    Created first API with spring boot. returns 'Hello' on port 8080

---
+++ mySpringBoot /src/main/java/com/hello/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+        SpringApplication.run(HelloWorld.class, args);
+    }
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello";
+    }
+}
